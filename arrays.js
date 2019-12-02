a51893d58a7120dbe5906bc5cf344fdd8a7bfc68
Thiago Augusto <thiago.btw@gmail.com>
2019-12-02 16:14:04 -0300

    exemplos de For While a ForEach

--- arrays.js
+++ arrays.js
@@ -11,3 +11,33 @@ console.log(videoGames.pop())
 console.log(videoGames.length)
 
 console.log(videoGames)
+
+// loop for
+for(var i = 0; i < 10; i++){
+    console.log(i)
+}
+
+//while
+var i = 0
+while(i < 10) {
+    console.log(i)
+    i++
+}
+
+var videoGamesOld = ['sega', 'snes', 'nes']
+
+//exemplo de for com break
+for(var i = 0; i <= videoGames.length; i++){
+    console.log(videoGamesOld[i])
+    if(videoGamesOld[i] === 'snes'){
+        break
+    }
+}
+
+// exemplo de Foreach
+
+var videoGamesNew = ['sega', 'snes', 'nes']
+
+videoGamesNew.forEach(function(item, index){
+    console.log(item, index)
+})
