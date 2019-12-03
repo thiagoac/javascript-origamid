746b838d91b72b4978913fd459ffdeb5e4543992
Thiago Augusto <thiago.btw@gmail.com>
2019-12-03 16:54:56 -0300

    Atribuição e ternario

---
+++ atribuicaoTernario.js
@@ -0,0 +1,24 @@
+var x = 1
+var y = 2
+
+console.log(x+=y)
+console.log(x)
+
+
+//Operador ternário - condição ? true : false
+var idade = 17
+var podeDirigir = (idade >= 18) ? 'Pode dirigir' : 'Não pode'
+
+console.log(podeDirigir)
+
+
+//exemplo de atribuicao
+var scroll = 1000
+scroll += 500
+console.log(scroll)
+
+
+var possuiCarro = true
+var possuiCasa = true
+var darCredito = (possuiCarro && possuiCasa)
+console.log(darCredito)
