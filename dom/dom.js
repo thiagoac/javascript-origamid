1f0e3ce499cfbf48f5b94f0e697678b83d0aebe9
Thiago Augusto <thiago.btw@gmail.com>
2019-12-05 08:55:39 -0300

    testando eventos

--- dom/dom.js
+++ dom/dom.js
@@ -1,7 +1,13 @@
 document.querySelector('h1').innerHTML = 'Teste'
 
 let btn = document.querySelector('button')
+let form = document.querySelector('input')
 
+/*
 btn.addEventListener('click',function(){
     document.querySelector('h1').innerHTML = 'Teste evento'
+})*/
+
+form.addEventListener('keyup',function(){
+    document.querySelector('h1').innerHTML = form.value
 })
