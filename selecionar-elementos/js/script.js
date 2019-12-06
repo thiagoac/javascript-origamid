16fc1787da0f5cd5862001a31e6dcc17869eab4f
Thiago Augusto <thiago.btw@gmail.com>
2019-12-06 16:47:46 -0300

    selecionando com o DOM

---
+++ selecionar-elementos/js/script.js
@@ -0,0 +1,21 @@
+// select per ID
+const animais = document.getElementById('animais')
+
+//select per Class
+const gridSection = document.getElementsByClassName('grid-section')
+
+//select per Tags
+const lista = document.getElementsByTagName('ul')
+
+//select only first Element
+const primeiraLista = document.querySelector('ul')
+
+const linkInterno = document.querySelector('[href="#faq"]')
+
+const linkInterno2 = document.querySelectorAll('a[href^="#faq"]')
+
+const animaisImagem = document.querySelectorAll('img[src^="img/imagem"]')
+
+const linkInterno3 = document.querySelectorAll('a[href^="#"]')
+
+console.log(animaisImagem)  
