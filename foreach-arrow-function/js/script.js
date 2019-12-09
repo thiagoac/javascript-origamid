f0db1fed3505cc984f9b2d997bd2a017669e2179
Thiago Augusto <thiago.btw@gmail.com>
2019-12-09 16:56:35 -0300

    exemplos de arrow functions

---
+++ foreach-arrow-function/js/script.js
@@ -0,0 +1,30 @@
+const img = document.querySelectorAll('img')
+
+const titulos = document.getElementsByClassName('titulos')
+
+const arrayTitulos = Array.from(titulos) // transforma um HTML Collection em array e pode ser lido pelo Foreach
+
+//console.log(titulos)
+
+//console.log(img)
+
+
+//funcio for each com callback
+img.forEach(function(item){
+    //console.log(item)
+})
+
+//exemplo de function
+arrayTitulos.forEach(function(item){
+    //console.log(item)
+})
+
+//Arrow Function
+arrayTitulos.forEach((item) => {
+    console.log(item)
+})
+
+//Arrow Function
+arrayTitulos.forEach((item) => {
+    console.log(item.innerHTML)
+})
